c3a0417d5a21eaac6c2f19fed2ffe33744b5ced6
Melissa Armand <melissarmand@gmail.com>
2021-09-05 21:40:35 -0400

    removed seeds file

--- seeders/seed.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require("mongoose");
-var db = require("../models");
-
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/imageperformance", {
-    useNewUrlParser: true
-});
-
